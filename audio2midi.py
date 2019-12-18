8ba62195532aee0e8407e0898c037a797966537b
Takamasa Iijima <takahyon@gmail.com>
2019-12-18 22:05:41 +0900

    fix

--- audio2midi.py
+++ audio2midi.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import argparse
-import tqdm
+from tqdm import tqdm
 
 def smoothing(roll):
 #     step1  Turn consecutively pitch labels into notes.
